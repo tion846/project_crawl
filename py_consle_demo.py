21839cd7976096003e348cf41cc0ac8aa7ae6655
Nathan <aasd609@yahoo.com.tw>
2023-09-19 23:24:39 +0800

    case: execute script, SimpleNamespace

--- py_consle_demo.py
+++ py_consle_demo.py
@@ -77,3 +77,58 @@ let data = await fetch(url)
 let result = await data.json()
 return result
 """
+
+# ’cp950′ codec can’t decode byte 0xe6 in position 111: illegal multibyte sequence
+# See https://www.wongwonggoods.com/all-posts/python/python-debug-error/cp950-codec-python/
+
+# dict to simplenamespace
+# See https://stackoverflow.com/questions/50296097/how-to-initialize-a-simplenamespace-from-a-dict
+
+import os
+import json
+from types import SimpleNamespace
+
+
+folder = "reference"
+file_name = "search.json"
+path = os.path.join(os.getcwd(), folder, file_name)
+
+with open(f"{path}", "r", encoding="utf-8") as file_json:
+    contnets = file_json.readlines()
+
+data = json.loads("".join(contnets),
+                  object_hook=lambda x: SimpleNamespace(**x))
+
+for doc in data.Results:
+    x = doc.Document
+    print([x.pdp_url, x.sale_price, x.product_name])
+
+# VisitId: cookie hawk_visit_id
+# VisitorId: cookie hawk_visitor_id
+# ClientGuid: ??
+
+js_script = """
+    let url = "https://essearchapi-na.hawksearch.com/api/v2/search";
+    let input = {
+        "query": "type:product",
+        "ClientData": {
+            "VisitId": "f4359327-1caf-4744-b575-30d553286896",
+            "VisitorId": "673cf168-236f-4757-8a0d-ab7aedfcc21b",
+            //"UserAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.31",
+        },
+        "PageNo": 2,
+        "Keyword": "Laptops",
+        "ClientGuid": "bdeebee3d2b74c8ea58522bb1db61f8e"
+    };
+    let headers = new Headers({
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+    });
+    let data = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(input),
+        headers :headers
+    });
+    let result = await data.json();
+    return result;
+"""
